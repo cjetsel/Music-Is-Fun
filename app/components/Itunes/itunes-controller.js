71fd37332acda40042bc42036f41b30e662dfad9
Chelsey Jetsel <crjetsel@gmail.com>
2019-05-19 20:11:53 -0600

    final

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -1,4 +1,5 @@
 import ItunesService from "./itunes-service.js";
+import Song from "../../models/Song.js";
 //Private
 const itunesService = new ItunesService()
 
@@ -8,14 +9,15 @@ function drawSongs() {
   for (let i = 0; i < currentSongs.length; i++) {
     let song = currentSongs[i];
     template += song.resultTemplate;
-
   }
   document.getElementById('song-list').innerHTML = template;
   //changes button back to GET MUSIC once songs are loaded
-  document.querySelector('#get-music-button').textContent = 'GET MUSIC'
+  document.querySelector('#get-music-button').innerHTML = `<i
+    class="fas fa-compact-disc"></i>`;
   console.log(itunesService.Songs)
-
 }
+
+
 
 
 //PUBLIC
@@ -31,7 +33,8 @@ class ItunesController {
     e.preventDefault();
     var artist = e.target.artist.value;
     //changes the button to loading while songs load
-    document.querySelector('#get-music-button').textContent = 'LOADING....'
+    document.querySelector('#get-music-button').innerHTML = `<i
+      class="fas fa-spin fa-compact-disc"></i>`
     itunesService.getMusicByArtist(artist)
   }
 }
--- app/models/Song.js
+++ app/models/Song.js
@@ -10,12 +10,31 @@ export default class Song {
     }
 
     get resultTemplate() {
-        return `<div class="col-6"> 
-        <img src = "${this.albumArt}">
-        <h4>${this.title}, ${this.artist}</h4> 
-        <audio controls>
-        <source src="${this.preview}">
-        </audio> 
+        return `<div class="col-12 song-preview"> 
+        <div class="row">
+        <div class="col-3 pl-0">
+        <img class="img-fluid rounded-circle tiny" src="${this.albumArt}"></div>
+        <div class ="col-9">
+        <div class="row d-flex align-content-center">
+        <div class="col-12 mt-4">
+        <h5><em>${this.title}</em></h5>
+        <h5>${this.artist}<h5>
+        <h5>$${this.price}<h5>   
+        </div>    
+        <div class="col-12 mb-4">
+        <audio controls><source src ="${this.preview}"></audio>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
         </div>`
     }
+
+    get playlistTemplate() {
+        return `<source src="${this.preview}">`
+    }
+
 }
+
+//title, artist, price, album art, preview (hint: "html audio tag"))
